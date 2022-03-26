26dfbd32fe0f83bfef81978bfe3a87945ffc7ec4
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-03-26 15:24:03 +0530

    add routes to the links on the navbar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@
 import './App.css';
 import Article from './components/Article';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm'
+import TextForm from './components/TextForm';
+import About from './components/About';
+// import {BrowserRouter as Router, Switch, Route,
+// Link } from "react-router-dom";
+// import { Route, Switch } from "react-router";
+// import { Routes ,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+
+
 function App() {
   return (
    <>
+   <Router>
    <div className="container">
      <Navbar title="TextUtils"/>
      {/* <Article /> */}
-     <TextForm heading="Enter the text below: "/>
+     <Routes>
+        <Route exact path="/" element={<TextForm heading="Enter the text below: "/> }/>
+        <Route exact path="/about" element={<About/>}/>
+     </Routes>
     </div>
+    </Router>
    </>
   );
 }
